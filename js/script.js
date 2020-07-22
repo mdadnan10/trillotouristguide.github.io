83edc975b4ac12e4b89dbbbceba5ec7f0d3854b7
mdadnan10 <57746276+mdadnan10@users.noreply.github.com>
2020-07-22 20:42:22 +0530

    my trillo-tourist-guide website

---
+++ js/script.js
@@ -0,0 +1,46 @@
+    /*-----side nav js--------*/
+    function openNav() {
+      document.getElementById("sidenav").style.width = "200px";
+      document.getElementById("main").style.marginLeft = "200px";
+    }
+
+    function closeNav() {
+      document.getElementById("sidenav").style.width = "0";
+      document.getElementById("main").style.marginLeft = "0";
+    }
+
+
+    /*-----international read more button js-----*/
+    var i = 0;
+
+    function read() {
+      if (!i) {
+        document.getElementById("more").style.display = "inline";
+        document.getElementById("dots").style.display = "none";
+        document.getElementById("read").innerHTML = "Read less";
+        i = 1;
+      } else {
+        document.getElementById("more").style.display = "none";
+        document.getElementById("dots").style.display = "inline";
+        document.getElementById("read").innerHTML = "Read more";
+        i = 0;
+      }
+    }
+
+
+    /*-----india read more button js-----*/
+    var i = 0;
+
+    function indianread() {
+      if (!i) {
+        document.getElementById("indianmore").style.display = "inline";
+        document.getElementById("indiandots").style.display = "none";
+        document.getElementById("indianread").innerHTML = "Read less";
+        i = 1;
+      } else {
+        document.getElementById("indianmore").style.display = "none";
+        document.getElementById("indiandots").style.display = "inline";
+        document.getElementById("indianread").innerHTML = "Read more";
+        i = 0;
+      }
+    }
